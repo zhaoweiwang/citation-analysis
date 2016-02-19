8faf3b4ad4a66727bd29b0d868b9a897722d0f70
zhaoweiwang <zhaowwang@gmail.com>
2016-02-19 18:49:30 +0800

    输出蛋白层次所有信息

---
+++ citation-analysis/citation-analysis/getMat.cpp
@@ -0,0 +1,198 @@
+#include "Head.h"
+#include <boost/tokenizer.hpp>
+
+extern vector<paperInfo> paperVec;
+
+set<string> stemListSet;
+vector<string> stemList;
+vector<string> stopWords;				//停用词集合
+
+unordered_map<string, int> stemCount;	//stem的文档数
+
+void loadStopWords()
+{
+	cout << "获取停用词." << endl << endl;
+	ifstream in("stop_words.txt");
+	string tmp;
+	while (!in.eof()) {
+		getline(in, tmp, '\n');
+		stopWords.push_back(tmp);
+		tmp.clear();
+	}
+	std::sort(stopWords.begin(), stopWords.end());
+}
+
+inline bool compTFIDF(const stemInfo &a, const stemInfo &b){
+	return a.tf_idf > b.tf_idf;
+}
+
+string str2Lower(string str)
+{
+	transform(str.begin(), str.end(), str.begin(), ::tolower);
+	return str;
+}
+
+
+void tfidf2vecMN(){
+	for (auto i = 0; i < paperVec.size(); ++i){
+		vector<double> dataTemp(stemList.size(), 0.0);
+		//int countTemp = 0;
+
+		for (auto j = 0; j < paperVec[i].stemVec.size(); j++){
+
+			size_t idx = find(stemList.begin(), stemList.end(), paperVec[i].stemVec[j].term) - stemList.begin();
+			if (idx == stemList.size())
+				continue;
+			else{
+				dataTemp.at(idx) = paperVec[i].stemVec[j].tf_idf;
+				//countTemp++;
+			}
+		}
+		dataTemp.swap(paperVec[i].dataMat);
+
+		//int countT = 0;
+		//for (int k = 0; k < paperVec[i].dataMat.size(); k++)
+		//if (paperVec[i].dataMat[k] != 0.0)
+		//	countT++;
+
+		//cout << countTemp << endl;
+		//cout << countT << endl;
+		//getchar();
+	}
+
+}
+
+void stemmingFunc(){
+	for (int i = 0; i < paperVec.size(); ++i){
+		//cout << paperVec[i].abstra << endl << endl;
+		//cout << "提取词条." << endl << endl;
+		boost::tokenizer<> tok(paperVec[i].abstra);
+
+		boost::tokenizer<> tokTitle(paperVec[i].title);
+		boost::tokenizer<> tokKeyword(paperVec[i].originalKeywords);
+ 
+		double countWord = 0.0;
+
+		for (boost::tokenizer<>::iterator beg = tok.begin(); beg != tok.end(); ++beg){
+			countWord++;
+
+			string strTemp = str2Lower(*beg);
+			wstring w(strTemp.size(), ' ');
+			copy(strTemp.begin(), strTemp.end(), w.begin());
+			stemming::english_stem<> english_stemmer;
+			english_stemmer(w);
+			string stem(strTemp.size(), ' ');
+			copy(w.begin(), w.end(), stem.begin());
+			stem = stem.substr(0, stem.find(' '));
+			if (!(std::binary_search(stopWords.begin(), stopWords.end(), stem))){
+				paperVec[i].wordVec[stem]++;
+				stemListSet.insert(stem);
+			}
+		}
+
+		for (boost::tokenizer<>::iterator beg = tokTitle.begin(); beg != tokTitle.end(); ++beg){
+			countWord++;
+			
+			string strTemp = str2Lower(*beg);
+			wstring w(strTemp.size(), ' ');
+			copy(strTemp.begin(), strTemp.end(), w.begin());
+			stemming::english_stem<> english_stemmer;
+			english_stemmer(w);
+			string stem(strTemp.size(), ' ');
+			copy(w.begin(), w.end(), stem.begin());
+			stem = stem.substr(0, stem.find(' '));
+			if (!(std::binary_search(stopWords.begin(), stopWords.end(), stem))){
+				paperVec[i].wordVec[stem]++;
+				stemListSet.insert(stem);
+
+				paperVec[i].stemTile.insert(stem);
+			}
+		}
+
+
+		for (boost::tokenizer<>::iterator beg = tokKeyword.begin(); beg != tokKeyword.end(); ++beg){
+			countWord++;
+
+			string strTemp = str2Lower(*beg);
+			wstring w(strTemp.size(), ' ');
+			copy(strTemp.begin(), strTemp.end(), w.begin());
+			stemming::english_stem<> english_stemmer;
+			english_stemmer(w);
+			string stem(strTemp.size(), ' ');
+			copy(w.begin(), w.end(), stem.begin());
+			stem = stem.substr(0, stem.find(' '));
+			if (!(std::binary_search(stopWords.begin(), stopWords.end(), stem))){
+				paperVec[i].wordVec[stem]++;
+				stemListSet.insert(stem);
+
+				paperVec[i].stemKeyword.insert(stem);
+			}
+		}
+
+
+		//cout << "词条小写化、去冗余处理." << endl << endl;
+		//cout << "进行词干还原." << endl << endl;
+		//getchar();
+
+		multimap<double, string> sortMap;
+		for (unordered_map<string, int>::iterator it = paperVec[i].wordVec.begin(); it != paperVec[i].wordVec.end(); ++it){
+			stemInfo stemTemp;
+			double tempTf = ((double)(it->second)) / countWord;
+			stemTemp.tf = tempTf;
+			stemTemp.term = it->first;
+			sortMap.insert(make_pair(tempTf, it->first));
+			stemCount[it->first]++;
+			paperVec[i].stemVec.push_back(stemTemp);
+		}
+
+
+		//for (multimap<double, string>::iterator it = sortMap.begin(); it != sortMap.end(); ++it)
+		//	cout << it->second << ":" << it->first << endl;
+		//cout << "总共：" << sortMap.size() << " 个词." << endl;
+		//getchar();
+
+		//unordered_map<string, int>::iterator iter;
+		//for (iter = paperVec[i].wordVec.begin(); iter != paperVec[i].wordVec.end(); ++iter)
+		//	cout << iter->first << ":" << iter->second << endl;
+		//getchar();
+	}
+
+	copy(stemListSet.begin(), stemListSet.end(), back_inserter(stemList));
+	//cout << stemList.size() << endl;
+	cout << "总共：" << stemCount.size() << "维." << endl;
+
+	//cout << "总共：" << stemList.size() << "维." << endl;
+	//getchar();
+
+	for (int i = 0; i < paperVec.size(); ++i){
+		for (int j = 0; j < paperVec[i].stemVec.size(); ++j){
+			paperVec[i].stemVec[j].idf = 1.0 / log(((double)paperVec.size() / (double)(stemCount[paperVec[i].stemVec[j].term])));
+			paperVec[i].stemVec[j].tf_idf = paperVec[i].stemVec[j].tf * paperVec[i].stemVec[j].idf;
+
+			double temp = paperVec[i].stemVec[j].tf_idf;
+			set<string>::iterator iter;
+			if ((iter = paperVec[i].stemTile.find(paperVec[i].stemVec[j].term)) != paperVec[i].stemTile.end())
+				paperVec[i].stemVec[j].tf_idf = 1.5 * temp;
+
+			if ((iter = paperVec[i].stemKeyword.find(paperVec[i].stemVec[j].term)) != paperVec[i].stemKeyword.end())
+				paperVec[i].stemVec[j].tf_idf = 2.5 * temp;
+		}
+
+		sort(paperVec[i].stemVec.begin(), paperVec[i].stemVec.end(), compTFIDF);
+
+		//for (int j = 0; j < paperVec[i].stemVec.size(); ++j){
+		//	cout << paperVec[i].stemVec[j].term << ":" << paperVec[i].stemVec[j].tf_idf << endl;
+		//}
+		//getchar();
+	}
+
+	tfidf2vecMN();
+
+}
+
+void getMat(){
+	cout << "Step3: 获得tf_idf矩阵." << endl;
+
+	loadStopWords();
+	stemmingFunc();
+}
---
+++ citation-analysis/citation-analysis/outputResult.cpp
@@ -0,0 +1,64 @@
+#include "Head.h"
+#include <iomanip>
+extern vector<paperInfo> paperVec;
+
+double calSimi(vector<double> &v1, vector<double> &v2){
+	double d1 = 0.0, d2 = 0.0, d3 = 0.0;
+	unsigned int len = v1.size();
+	for (unsigned int i = 0; i < len; ++i){
+		d1 += (v1[i] * v2[i]);
+		d2 += (v1[i] * v1[i]);
+		d3 += (v2[i] * v2[i]);
+	}
+	//cout << d1 << " " << d2 << " " << d3 << endl;
+	//cout << d1 / (sqrt(d2) * sqrt(d3)) << endl;
+	//getchar();
+	return d1 / (sqrt(d2) * sqrt(d3));
+}
+
+double calVector(vector<double> &a){
+	double result = 0.0;
+	for (unsigned int i = 0; i < a.size(); ++i)
+		result += (a[i] * a[i]);
+	return result;
+}
+
+void outputResult(){
+	ofstream out("result_unweighted.txt");
+	int countScan = 0;
+	int countEdge = 0;
+	for (auto i = 0; i < paperVec.size(); ++i){
+		if (paperVec[i].citeFlag){
+			paperVec[i].scan = ++countScan;
+			if (!paperVec[i].citeRidx.empty())
+				countEdge += paperVec[i].citeRidx.size();
+		}
+	}
+
+	out << "*Vertices " << countScan << endl;
+	for (auto i = 0; i < paperVec.size(); ++i){
+		if (paperVec[i].citeFlag)
+			out << paperVec[i].scan << " " <<  "\"" << paperVec[i].title << "\"" << endl;
+	}
+
+	out << "*Edges " << countEdge << endl;
+	for (auto i = 0; i < paperVec.size(); ++i){
+		if (!paperVec[i].citeRidx.empty()){
+			for (auto j = 0; j < paperVec[i].citeRidx.size(); ++j){
+				out << paperVec[i].scan << " " << paperVec[paperVec[i].citeRidx[j]].scan << " " << setiosflags(ios::fixed) << setprecision(7) << calSimi(paperVec[i].dataMat, paperVec[paperVec[i].citeRidx[j]].dataMat) << endl;
+
+				if (calVector(paperVec[paperVec[i].citeRidx[j]].dataMat) == 0.0){
+					cout << calVector(paperVec[i].dataMat) << endl;
+					cout << paperVec[paperVec[i].citeRidx[j]].stemVec.size() << endl;
+					cout << paperVec[paperVec[i].citeRidx[j]].title << endl;
+					cout << paperVec[paperVec[i].citeRidx[j]].abstra << endl;
+					//for (int i )
+					getchar();
+					
+				}
+			}
+		}
+	}
+
+	out.close();
+}
